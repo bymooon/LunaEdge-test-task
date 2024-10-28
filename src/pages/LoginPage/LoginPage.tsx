335ebddaaebe06f0ddc197e50783abb519538577
bymooon <antonbakhtin1607@gmail.com>
2024-10-28 20:26:40 +0200

    added animations for step progress

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,11 +3,20 @@ import styles from "./LoginPage.module.css";
 import chadLogo from "../../assets/chadLogo.png";
 import iconEye from "../../assets/iconEye.svg";
 import iconEyeOff from "../../assets/iconEyeOff.svg";
-import ConnectShopify from "../../components/ConnectShopify/ConnectShopify";
+import ConnectShopify from "../../components/ShopifyConnection/ConnectShopify/ConnectShopify";
 
 const LoginPage: React.FC = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [currentStep, setCurrentStep] = useState(1);
+  const dots = Array(5).fill(0);
+
+  const handleNext = () => {
+    if (currentStep < 5) setCurrentStep((prevStep) => prevStep + 1);
+  };
+
+  const handleBack = () => {
+    if (currentStep > 1) setCurrentStep((prevStep) => prevStep - 1);
+  };
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
@@ -18,31 +27,82 @@ const LoginPage: React.FC = () => {
     setCurrentStep(2);
   };
 
+  const CheckIcon = () => (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20.285 5.955a1 1 0 00-1.414 0L9 15.828 5.13 11.958a1 1 0 10-1.414 1.414l4.95 4.95a1 1 0 001.414 0l10.205-10.204a1 1 0 000-1.414z"
+        fill="#FFFFFF"
+      />
+    </svg>
+  );
+
   return (
     <div className={styles["container"]}>
       <div className={styles["progress-container"]}>
         <div className={styles["progress-item"]}>
-          <div className={styles["circle"]}></div>
+          <div
+            className={`${styles["circle"]} 
+            ${currentStep === 1 ? styles["current-circle"] : ""}
+            ${currentStep > 1 ? styles["active-circle"] : ""}`}
+          >
+            {currentStep > 1 && <CheckIcon />}
+          </div>
           <p>Welcome</p>
         </div>
-        <div className={styles["line"]} />
+        <div
+          className={`${styles["line"]} ${
+            currentStep >= 2 ? styles["active-line"] : ""
+          }`}
+        />
         <div className={styles["progress-item"]}>
-          <div className={styles["circle"]}></div>
+          <div
+            className={`${styles["circle"]} 
+            ${currentStep === 2 ? styles["current-circle"] : ""}
+            ${currentStep > 2 ? styles["active-circle"] : ""}`}
+          >
+            {currentStep > 2 && <CheckIcon />}
+          </div>
           <p>Connect your Shopify store</p>
         </div>
-        <div className={styles["line"]} />
+        <div
+          className={`${styles["line"]} ${
+            currentStep >= 3 ? styles["active-line"] : ""
+          }`}
+        />
         <div className={styles["progress-item"]}>
-          <div className={styles["circle"]}></div>
-          <h2>Connect your customer support email</h2>
+          <div
+            className={`${styles["circle"]} 
+            ${currentStep === 3 ? styles["current-circle"] : ""}
+            ${currentStep > 3 ? styles["active-circle"] : ""}`}
+          >
+            {currentStep > 3 && <CheckIcon />}
+          </div>
+          <p>Connect your customer support email</p>
         </div>
-        <div className={styles["line"]} />
+        <div
+          className={`${styles["line"]} ${
+            currentStep >= 4 ? styles["active-line"] : ""
+          }`}
+        />
         <div className={styles["progress-item"]}>
-          <div className={styles["circle"]}></div>
+          <div
+            className={`${styles["circle"]} 
+            ${currentStep === 4 ? styles["current-circle"] : ""}
+            ${currentStep > 4 ? styles["active-circle"] : ""}`}
+          >
+            {currentStep > 4 && <CheckIcon />}
+          </div>
           <p>Done</p>
         </div>
         <div className={styles["progress-controls"]}>
-          <button>&lt; Back</button>
-          <button>Next &gt;</button>
+          <button onClick={handleBack}>&lt; Back</button>
+          <button onClick={handleNext}>Next &gt;</button>
         </div>
         <div className={styles["onboarding-stats-container"]}>
           <div className={styles["onboarding-stats-box"]}>
@@ -53,13 +113,11 @@ const LoginPage: React.FC = () => {
             </p>
           </div>
         </div>
-        <div className={styles["dots-container"]}>
-          <div className={styles["dot"]}></div>
-          <div className={styles["dot"]}></div>
-          <div className={styles["dot"]}></div>
-          <div className={styles["dot"]}></div>
-          <div className={styles["dot"]}></div>
-        </div>
+        <ul className={styles["dots-container"]}>
+          {dots.map((_, index) => (
+            <li key={index} className={styles["dot"]} />
+          ))}
+        </ul>
       </div>
       <div className={styles["login-container"]}>
         {currentStep === 1 && (
@@ -130,7 +188,7 @@ const LoginPage: React.FC = () => {
             </div>
           </div>
         )}
-        {currentStep === 2 && <ConnectShopify />}
+        {currentStep === 2 && <ConnectShopify handleNext={handleNext} />}
       </div>
     </div>
   );
