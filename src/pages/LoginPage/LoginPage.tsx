09a802b6aae8170a868f0a425c38942f0e1e8ba3
bymooon <antonbakhtin1607@gmail.com>
2024-10-28 14:05:06 +0200

    added shopify step for login

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,12 +3,19 @@ import styles from "./LoginPage.module.css";
 import chadLogo from "../../assets/chadLogo.png";
 import iconEye from "../../assets/iconEye.svg";
 import iconEyeOff from "../../assets/iconEyeOff.svg";
+import ConnectShopify from "../../components/ConnectShopify/ConnectShopify";
 
 const LoginPage: React.FC = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [currentStep, setCurrentStep] = useState(1);
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setCurrentStep(2);
   };
 
   return (
@@ -62,72 +69,75 @@ const LoginPage: React.FC = () => {
         </div>
       </div>
       <div className={styles["login-container"]}>
-        <div className={styles["login-box"]}>
-          <div className={styles["title-box"]}>
-            <img src={chadLogo} alt="Logo" />
-            <h2 className={styles["login-title"]}>Chad</h2>
-          </div>
-          <p className={styles["login-welcome"]}>Welcome to Chad</p>
-          <p className={styles["login-paragraph"]}>
-            Go live in 10 minutes! Our self-service widget empowers your
-            customers to manage orders and track shipments 24/7 without driving
-            you crazy.
-          </p>
-          <div className={styles["form-container"]}>
-            <form>
-              <div className={styles["form-group"]}>
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  required
-                  placeholder="megachad@trychad.com"
-                />
-              </div>
-
-              <div className={styles["form-group"]}>
-                <label htmlFor="name">Name</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  required
-                  placeholder="Mega Chad"
-                />
-              </div>
-
-              <div className={styles["form-group"]}>
-                <label htmlFor="password">Password</label>
-                <div className={styles["password-container"]}>
+        {currentStep === 1 && (
+          <div className={styles["login-box"]}>
+            <div className={styles["title-box"]}>
+              <img src={chadLogo} alt="Logo" />
+              <h2 className={styles["login-title"]}>Chad</h2>
+            </div>
+            <p className={styles["login-welcome"]}>Welcome to Chad</p>
+            <p className={styles["login-paragraph"]}>
+              Go live in 10 minutes! Our self-service widget empowers your
+              customers to manage orders and track shipments 24/7 without
+              driving you crazy.
+            </p>
+            <div className={styles["form-container"]}>
+              <form onSubmit={handleFormSubmit}>
+                <div className={styles["form-group"]}>
+                  <label htmlFor="email">Email</label>
                   <input
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    name="password"
+                    type="email"
+                    id="email"
+                    name="email"
                     required
-                    placeholder="Enter password"
+                    placeholder="megachad@trychad.com"
                   />
-                  <span
-                    className={styles["show-password"]}
-                    onClick={togglePasswordVisibility}
-                  >
-                    <img
-                      src={showPassword ? iconEyeOff : iconEye}
-                      alt="Toggle visibility"
-                    />
-                  </span>
                 </div>
-              </div>
 
-              <button type="submit" className={styles["submit-button"]}>
-                Create account
-              </button>
-            </form>
-            <h3 className={styles["login-question"]}>
-              Already have an account? <a href="#">Login</a>
-            </h3>
+                <div className={styles["form-group"]}>
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    required
+                    placeholder="Mega Chad"
+                  />
+                </div>
+
+                <div className={styles["form-group"]}>
+                  <label htmlFor="password">Password</label>
+                  <div className={styles["password-container"]}>
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      id="password"
+                      name="password"
+                      required
+                      placeholder="Enter password"
+                    />
+                    <span
+                      className={styles["show-password"]}
+                      onClick={togglePasswordVisibility}
+                    >
+                      <img
+                        src={showPassword ? iconEyeOff : iconEye}
+                        alt="Toggle visibility"
+                      />
+                    </span>
+                  </div>
+                </div>
+
+                <button type="submit" className={styles["submit-button"]}>
+                  Create account
+                </button>
+              </form>
+              <h3 className={styles["login-question"]}>
+                Already have an account? <a href="#">Login</a>
+              </h3>
+            </div>
           </div>
-        </div>
+        )}
+        {currentStep === 2 && <ConnectShopify />}
       </div>
     </div>
   );
