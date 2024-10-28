c81a50e5d4c4bcde936aff7253a7468e57f8239f
bymooon <antonbakhtin1607@gmail.com>
2024-10-28 23:31:53 +0200

    created main page

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import styles from "./MainPage.module.css";
+const MainPage: React.FC = () => {
+  return (
+    <div className={styles["main-page"]}>
+      <p className={styles["main-text"]}> Hello Luna Edge, My name is Anton.</p>
+    </div>
+  );
+};
+
+export default MainPage;
