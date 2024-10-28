f45be113cef08373fe2847616eb38458387d3866
bymooon <antonbakhtin1607@gmail.com>
2024-10-28 23:32:47 +0200

    added route to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </Router>
   );
