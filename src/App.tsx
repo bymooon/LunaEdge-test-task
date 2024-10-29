96e0ee4ee5283ee30370f90330b3b053e86b9ded
bymooon <antonbakhtin1607@gmail.com>
2024-10-29 21:06:14 +0200

    added provider for app

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MainPage from "./pages/MainPage/MainPage";
+import { AuthProvider } from "./context/AuthContext";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
