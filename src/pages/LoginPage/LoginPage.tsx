ec3992c4f40c37ccadbdb062d0498d3b3d956135
bymooon <antonbakhtin1607@gmail.com>
2024-10-29 21:44:14 +0200

    fixed box styles

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -9,8 +9,8 @@ import { useMediaQuery } from "../../hooks/useMediaQuery";
 const LoginPage: React.FC = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [completedSteps, setCompletedSteps] = useState([false, false, false]);
-  // const isMobile = useMediaQuery("(max-width: 768px)");
-  const isMobile = useMediaQuery("(max-width: 1194px)");
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const isDeskopLoginProgressHidden = useMediaQuery("(max-width: 1194px)");
   // const isTablet = useMediaQuery("(min-width: 769px) and (max-width: 1194px)");
 
   const handleNext = () => {
@@ -30,7 +30,7 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className={styles["container"]}>
-      {!isMobile && (
+      {!isDeskopLoginProgressHidden && (
         <LoginProgress
           currentStep={currentStep}
           handleNext={handleNext}
@@ -39,7 +39,13 @@ const LoginPage: React.FC = () => {
         />
       )}
 
-      <div className={styles["login-container"]}>
+      <div
+        className={
+          isMobile
+            ? styles["mobile-login-container"]
+            : styles["login-container"]
+        }
+      >
         {currentStep === 1 && <LoginForm handleNext={handleNext} />}
         {currentStep === 2 && <ConnectShopify handleNext={handleNext} />}
         {currentStep === 3 && <ConnectGmail />}
